095b04f7df9f1595d9953dfb1b87183aaf20ebe0
rohitkumar-aiDev <33428077+rohitkumar-aiDev@users.noreply.github.com>
2025-08-12 16:59:25 +0530

    Add files via upload
    Music library

---
+++ musicLibrary.py
@@ -0,0 +1,15 @@
+music = {
+    "aaditya": "https://www.youtube.com/watch?v=dlhNpCveGeE",
+    "kanakdhara": "https://www.youtube.com/watch?v=3Y1JGvieOBk",
+    "ganesh arthav": "https://www.youtube.com/watch?v=VpATv_6pCPE",
+    "shiv daridra dahan": "https://www.youtube.com/watch?v=pgHEdDbUAAE&list=RDpgHEdDbUAAE&start_radio=1",
+    "ram raksha stotra": "https://www.youtube.com/watch?v=-_axSlApc98&list=RD-_axSlApc98&start_radio=1",
+    "vishnu sahasranam": "https://www.youtube.com/watch?v=AIOTIR7Awb4&list=RDAIOTIR7Awb4&start_radio=1",
+    "medha suktam": "https://www.youtube.com/watch?v=EwTppFPdFV4&list=RDEwTppFPdFV4&start_radio=1&pp=ygUMbWVkaGEgc3VrdGFtoAcB",
+    "namamish mishan": "https://www.youtube.com/watch?v=dSw_CnG-9rI&list=RDdSw_CnG-9rI&start_radio=1",
+    "krishna": "https://www.youtube.com/watch?v=TmJuQUX6SNY&list=RDTmJuQUX6SNY&start_radio=1&t=143s",
+    "giri nandini": "https://www.youtube.com/watch?v=442ewPgXHQ0&list=RD442ewPgXHQ0&start_radio=1",
+    "bhagya suktam": "https://www.youtube.com/watch?v=Hw2T1N5vlmc&list=RDHw2T1N5vlmc&start_radio=1",
+    "hanuman strotra": "https://www.youtube.com/watch?v=StpQXIc5rPo&list=RDStpQXIc5rPo&start_radio=1",
+    "raksha-bandhan": "https://www.youtube.com/watch?v=vy5okAVIFws&list=RDvy5okAVIFws&start_radio=1"
+}
